489e5c4c58de99999169c87b2e7ff73f399366c5
Jamie Kaczor <dignanjk@aol.com>
2021-11-18 12:55:46 -0500

    player class

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-renderer.setClearColor( 0xb7c3f3, 1 );
+renderer.setClearColor(0xb7c3f3, 1);
 
-const light = new THREE.AmbientLight( 0xffffff ); 
-scene.add( light );
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
 
 // global variables
 const start_position = 3
@@ -16,11 +16,11 @@ const end_position = -start_position
 
 function createCube(size, positionX, rotY = 0, color = 0xfbc851) {
     const geometry = new THREE.BoxGeometry(size.w, size.h, size.d);
-    const material = new THREE.MeshBasicMaterial( { color: color } );
-    const cube = new THREE.Mesh( geometry, material );
+    const material = new THREE.MeshBasicMaterial({ color: color });
+    const cube = new THREE.Mesh(geometry, material);
     cube.position.x = positionX;
     cube.rotation.y = rotY;
-    scene.add( cube );
+    scene.add(cube);
     return cube;
 }
 
@@ -31,7 +31,7 @@ const loader = new THREE.GLTFLoader();
 class Doll {
     constructor() {
         loader.load("../models/scene.gltf", (gltf) => {
-            scene.add( gltf.scene );
+            scene.add(gltf.scene);
             gltf.scene.scale.set(.4, .4, .4);
             gltf.scene.position.set(0, -1, 0);
             this.doll = gltf.scene;
@@ -40,37 +40,66 @@ class Doll {
 
     lookBackward() {
         // this.doll.rotation.y = -3.15
-        gsap.to(this.doll.rotation, {y: -3.15, duration: .45})
+        gsap.to(this.doll.rotation, { y: -3.15, duration: .45 })
     }
 
     lookForward() {
-        gsap.to(this.doll.rotation, {y: 0, duration: .45})
+        gsap.to(this.doll.rotation, { y: 0, duration: .45 })
     }
 }
 
 function createTrack() {
-    createCube({w: start_position * 2 + .2, h: 1.5, d: 1}, 0, 0, 0xe5a716).position.z = -1;
-    createCube({w: .2, h: 1.5, d: 1}, start_position, -.35);
-    createCube({w: .2, h: 1.5, d: 1}, end_position, .35);    
+    createCube({ w: start_position * 2 + .2, h: 1.5, d: 1 }, 0, 0, 0xe5a716).position.z = -1;
+    createCube({ w: .2, h: 1.5, d: 1 }, start_position, -.35);
+    createCube({ w: .2, h: 1.5, d: 1 }, end_position, .35);
 }
 createTrack()
 
+class Player {
+    constructor() {
+        const geometry = new THREE.SphereGeometry(.3, 32, 16);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        const sphere = new THREE.Mesh(geometry, material);
+        sphere.position.z = 1;
+        sphere.position.x = start_position;
+        scene.add(sphere);
+        this.player = sphere
+        this.playerInfo = {
+            positionX: start_position,
+            velocity: 0.1
+        }
+    }
+
+    run() {
+        this.playerInfo.velocity = .03
+    }
+
+    update() {
+        this.playerInfo.positionX -= this.playerInfo.velocity
+        this.player.position.x = this.playerInfo.positionX
+    }
+}
+
+const player = new Player()
 let doll = new Doll()
+
 setTimeout(() => {
     doll.lookBackward()
 }, 1000);
 
 function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+    renderer.render( scene, camera );
+    requestAnimationFrame( animate );
+    player.update();
 }
 animate();
 
-window.addEventListener( 'resize', onWindowResize, false);
+window.addEventListener('resize', onWindowResize, false);
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
 }
+
